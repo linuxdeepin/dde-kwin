bbeb4ba04667e87070ab358f826fa00981e8c48a
zccrs <ccrr1314@live.com>
2019-03-22 10:40:00 +0800

    feat: add kwin-xcb platform plugin
    hook kwin progrom by this plugin
    add "__dde__" object for "workspace" object on the scripts(js/qml)

---
+++ plugins/kwin-xcb/main.cpp
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2017 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     zccrs <zccrs@live.com>
+ *
+ * Maintainer: zccrs <zhangjide@deepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <qpa/qplatformintegrationplugin.h>
+#include <qpa/qplatformintegrationfactory_p.h>
+#include <qpa/qplatformintegration.h>
+#include <private/qguiapplication_p.h>
+
+#include <QDebug>
+#include <QLibrary>
+#include <QProcess>
+#include <QPluginLoader>
+#include <QDir>
+
+#include "vtablehook.h"
+
+QT_BEGIN_NAMESPACE
+
+static QObject *findObjectByClassName(const QByteArray &name, const QObjectList &list)
+{
+    foreach (QObject *obj, list) {
+        if (obj->metaObject()->className() == name) {
+            return obj;
+        }
+    }
+
+    return nullptr;
+}
+
+namespace KWin {
+class Workspace : public QObject {
+public:
+    static Workspace *_self;
+};
+class Scripting : public QObject {
+public:
+    static Scripting *s_self;
+};
+class Toplevel : public QObject {
+
+};
+class AbstractClient : public Toplevel {
+
+};
+class Client : public AbstractClient {
+public:
+    static const QMetaObject staticMetaObject;
+};
+}
+
+class Mischievous;
+class  Mischievous : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Mischievous() {
+        self = this;
+    }
+
+    Q_INVOKABLE QObject *require(const QString &module)
+    {
+        if (QObject *obj = moduleMap.value(module)) {
+            return obj;
+        }
+
+        QString file = module;
+        bool isFile = QFile::exists(file);
+
+        if (!isFile) {
+            static QStringList pluginPaths {
+                QDir::home().absoluteFilePath(QStringLiteral(".local/lib/kwin-xcb/plugins")),
+                QStringLiteral("/usr/lib/kwin-xcb/plugins")
+            };
+
+            for (const QString &path : pluginPaths) {
+                QDir dir(path);
+
+                file.prepend("lib");
+                file.append(".so");
+                file = dir.absoluteFilePath(file);
+                isFile = QFile::exists(file);
+
+                if (isFile) {
+                    break;
+                }
+            }
+        }
+
+        if (!isFile) {
+            return nullptr;
+        }
+
+        QPluginLoader loader(file);
+
+        if (!loader.load()) {
+            qWarning() << Q_FUNC_INFO << loader.errorString();
+            return nullptr;
+        }
+
+        QObject *obj = loader.instance();
+        moduleMap[module] = obj;
+
+        if (obj) {
+            obj->metaObject()->invokeMethod(obj, "init", Q_ARG(QObject*, this));
+        }
+
+        return obj;
+    }
+
+    Q_INVOKABLE int execute(const QString &program, const QStringList &arguments, const QString &workingDirectory = QString())
+    {
+        QProcess p;
+
+        p.setProgram(program);
+        p.setArguments(arguments);
+        p.setWorkingDirectory(workingDirectory);
+
+        p.start(QIODevice::ReadOnly);
+        p.waitForFinished();
+
+        return p.exitCode();
+    }
+    Q_INVOKABLE int execute(const QString &command, const QString &workingDirectory = QString())
+    {
+        QProcess p;
+
+        p.setWorkingDirectory(workingDirectory);
+        p.start(command, QIODevice::ReadOnly);
+        p.waitForFinished();
+
+        return p.exitCode();
+    }
+    Q_INVOKABLE bool startDetached(const QString &program, const QStringList &arguments, const QString &workingDirectory = QString())
+    {
+        return QProcess::startDetached(program, arguments, workingDirectory);
+    }
+    Q_INVOKABLE bool startDetached(const QString &command)
+    {
+        return QProcess::startDetached(command);
+    }
+
+public slots:
+    void init() {
+        if (!KWin::Scripting::s_self)
+            return;
+
+        const QObjectList &scripting_children = KWin::Scripting::s_self->children();
+        QObject *jsWorkspaceWrapper = findObjectByClassName(QByteArrayLiteral("KWin::QtScriptWorkspaceWrapper"), scripting_children);
+        QObject *qmlWorkspaceWrapper = findObjectByClassName(QByteArrayLiteral("KWin::DeclarativeScriptWorkspaceWrapper"), scripting_children);
+
+        // 给js脚本引擎添加对象
+        if (jsWorkspaceWrapper) {
+            jsWorkspaceWrapper->setProperty("__dde__", QVariant::fromValue(this));
+        }
+
+        // 给qml脚本引擎添加对象
+        if (qmlWorkspaceWrapper) {
+            qmlWorkspaceWrapper->setProperty("__dde__", QVariant::fromValue(this));
+        }
+    }
+
+public:
+    static Mischievous *self;
+    QMap<QString, QObject*> moduleMap;
+};
+
+Mischievous *Mischievous::self = nullptr;
+Q_GLOBAL_STATIC(Mischievous, _m)
+
+static void overrideInitialize(QPlatformIntegration *i)
+{
+    // kwin中只允许使用名称为"xcb"插件
+    *QGuiApplicationPrivate::platform_name = "xcb";
+    VtableHook::callOriginalFun(i, &QPlatformIntegration::initialize);
+}
+
+class DPlatformIntegrationPlugin : public QPlatformIntegrationPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID QPlatformIntegrationFactoryInterface_iid FILE "kwin-xcb.json")
+
+public:
+    QPlatformIntegration *create(const QString&, const QStringList&, int &, char **) Q_DECL_OVERRIDE;
+};
+
+QPlatformIntegration* DPlatformIntegrationPlugin::create(const QString& system, const QStringList& parameters, int &argc, char **argv)
+{
+    if (system == "kwin-xcb") {
+        QPlatformIntegration *integration = QPlatformIntegrationFactory::create("xcb", parameters, argc, argv, PLATFORMS_PLUGIN_PATH);
+        VtableHook::overrideVfptrFun(integration, &QPlatformIntegration::initialize, overrideInitialize);
+
+        metaObject()->invokeMethod(this, [] {
+            connect(qApp, SIGNAL(workspaceCreated()), _m.operator ->(), SLOT(init()));
+        }, Qt::QueuedConnection);
+
+        return integration;
+    }
+
+    return 0;
+}
+
+QT_END_NAMESPACE
+
+#include "main.moc"
