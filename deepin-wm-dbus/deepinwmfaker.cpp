bbaa8216cf98126b01f6080dff3f6fa18a48bd74
wangxinbo <wangxinbo@uniontech.com>
2020-09-09 10:41:56 +0800

    fix: Provides a default wallpaper for the second workspace.
    Provides a default wallpaper for the second workspace.
    and compatible sp1 to sp2's environment.And mips platform
    wallpaper is different to other platform
    
    Log: Provides a default wallpaper for the second workspace.
    Bug: https://pms.uniontech.com/zentao/bug-view-11135.html
    Task: https://pms.uniontech.com/zentao/task-view-41341.html
    Change-Id: I43030a473a010fe4d85fa2c73c46b1a94af402cf

--- deepin-wm-dbus/deepinwmfaker.cpp
+++ deepin-wm-dbus/deepinwmfaker.cpp
@@ -44,7 +44,12 @@ Q_GLOBAL_STATIC_WITH_ARGS(QGSettings, _gsettings_dde_zone, ("com.deepin.dde.zone
 #define KWinDBusPath "/KWin"
 #define KWinDBusCompositorInterface "org.kde.kwin.Compositing"
 #define KWinDBusCompositorPath "/Compositor"
-const char fallback_background_name[] = "file:///usr/share/backgrounds/default_background.jpg";
+const char defaultFirstBackgroundUri[] = "file:///usr/share/backgrounds/default_background.jpg";
+#ifdef __mips__
+    const char defaultSecondBackgroundUri[] = "file:///usr/share/wallpapers/deepin/francesco-ungaro-1fzbUyzsHV8-unsplash.bmp";
+#else
+    const char defaultSecondBackgroundUri[] = "file:///usr/share/wallpapers/deepin/francesco-ungaro-1fzbUyzsHV8-unsplash.jpg";
+#endif
 
 using org::kde::KWin;
 
@@ -298,10 +303,18 @@ QString DeepinWMFaker::GetWorkspaceBackgroundForMonitor(const int index,const QS
 {
     QUrl uri = getWorkspaceBackgroundForMonitor(index, strMonitorName);
     if (uri.isEmpty()) {
-        uri = fallback_background_name;
-        if (!QFileInfo(uri.path()).isFile()) {
-            uri = GetWorkspaceBackground(1);
+        uri = _gsettings_dde_appearance->get(GsettingsBackgroundUri).toStringList().value(index - 1);
+        if (index == 1) {
+            if(!QFileInfo(uri.path()).isFile()) {
+                uri = defaultFirstBackgroundUri;
+            }
+        } else {
+            if (!QFileInfo(uri.path()).isFile()) {
+                uri = defaultSecondBackgroundUri;
+            }
         }
+        const QString &workSpaceBackgroundUri = uri.toString();
+        setWorkspaceBackgroundForMonitor(index, strMonitorName, workSpaceBackgroundUri);
     }
     return uri.toString();
 }
@@ -971,14 +984,24 @@ QString DeepinWMFaker::getWorkspaceBackgroundForMonitor(const int index, const Q
 {
     return  m_deepinWMConfig->group("WorkspaceBackground").readEntry( QString("%1%2%3").arg(index).arg("@" ,strMonitorName)) ;
 }
-void DeepinWMFaker::setWorkspaceBackgroundForMonitor(const int index, const QString &strMonitorName, const QString &uri)
+void DeepinWMFaker::setWorkspaceBackgroundForMonitor(const int index, const QString &strMonitorName, const QString &uri) const
 {
     m_deepinWMWorkspaceBackgroundGroup->writeEntry(QString("%1%2%3").arg(index).arg("@" ,strMonitorName), uri);
-    Q_EMIT WorkspaceBackgroundChangedForMonitor( index ,strMonitorName,uri );
     m_deepinWMConfig->sync();
 
 #ifndef DISABLE_DEEPIN_WM
-    _gsettings_dde_appearance->set(GsettingsBackgroundUri, uri);
+    QStringList allWallpaper = _gsettings_dde_appearance->get(GsettingsBackgroundUri).toStringList();
+
+    if (index > allWallpaper.size()) {
+        allWallpaper.reserve(index);
+
+        for (int i = allWallpaper.size(); i < index; ++i) {
+            allWallpaper.append(QString());
+        }
+    }
+
+    allWallpaper[index - 1] = uri;
+    _gsettings_dde_appearance->set(GsettingsBackgroundUri, allWallpaper);
 #endif // DISABLE_DEEPIN_WM
 }
 
