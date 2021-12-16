f953b566e138d87d7cac54fc46d908f2c86d758b
xupeidong <xupeidong@uniontech.com>
2021-12-16 17:51:08 +0800

    feat: add splitmenu translate
    add splitmenu translate
    
    Log: add splitmenu translate
    Bug: https://pms.uniontech.com/zentao/bug-view-108881.html
    Change-Id: I560d069d3de523b706e755f65c33ecc5871a1308

---
+++ plugins/kdecoration/translations/splitmenu_zh_HK.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="zh_HK">
+<context>
+    <name>ChameleonSplitMenu</name>
+    <message>
+        <source>Tile window to left of screen</source>
+        <translation>將窗口拼貼到左側</translation>
+    </message>
+    <message>
+        <source>Tile window to right of screen</source>
+        <translation>將窗口拼貼到右側</translation>
+    </message>
+    <message>
+        <source>Maximize</source>
+        <translation>最大化</translation>
+    </message>
+    <message>
+        <source>Unmaximize</source>
+        <translation>還原</translation>
+    </message>
+</context>
+</TS>
