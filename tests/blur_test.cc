fc5cddba2327e5540ef663f42d5d93e93f89f6a2
justforlxz <zhangdingyuan@deepin.org>
2022-09-21 09:58:49 +0800

    add try_compile

---
+++ tests/blur_test.cc
@@ -0,0 +1,11 @@
+#include <kwineffects.h>
+#include <kwinglplatform.h>
+#include <kwinglutils.h>
+
+#include <QVector>
+
+int main(int argc, char *argv[])
+{
+    QVector<GLRenderTarget *> m_renderTargets;
+    return 0;
+}
---
+++ tests/wayland_test.cc
@@ -0,0 +1,6 @@
+#include <KWayland/Server/ddeshell_interface.h>
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
