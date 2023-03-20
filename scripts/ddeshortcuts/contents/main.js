c309f3e06fb8223e5bc7afd6ea52a52ae1447045
ck <chenke@uniontech.com>
2023-03-20 17:50:05 +0800

    fix: ddeshortcuts not work
    1. change plugin installed path
    2. update js function
    
    Issue: https://github.com/linuxdeepin/developer-center/issues/3640

--- scripts/ddeshortcuts/contents/main.js
+++ scripts/ddeshortcuts/contents/main.js
@@ -1,8 +1,8 @@
-if (workspace.__dde__) {
+//if (workspace.__dde__) {
     registerShortcut("Window Absolute Maximize", "Window Absolute Maximize", "Meta+Up", function() {
-        workspace.__dde__.kwinUtils.fullmaximizeWindow(workspace.activeClient);
+        workspace.activeClient.setMaximize(true, true);
     })
     registerShortcut("Window Unmaximize", "Window Unmaximize", "Meta+Down", function() {
-        workspace.__dde__.kwinUtils.unmaximizeWindow(workspace.activeClient);
+        workspace.activeClient.setMaximize(false, false);
     })
-}
+//}
